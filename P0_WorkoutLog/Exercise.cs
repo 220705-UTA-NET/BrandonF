40334a12903175c113a7a049ae517dfc28f378df
Brandon <figueredo407@gmail.com>
2022-07-15 19:35:38 -0400

    added Workout and Cardio classes

--- P0_WorkoutLog/Exercise.cs
+++ P0_WorkoutLog/Exercise.cs
@@ -10,24 +10,65 @@ namespace Exercises
         Hard
     }
 
-    public struct Duration
-    {
-        public string StartTime; // HH:MM
-        public string EndTime; // HH:MM
-    }
-
     abstract class Exercise
     {
-
         public string? Name;
         public string? Type;
+        public string StartTime { get; set; } // HH:MM
+        public string EndTime { get; set; } // HH:MM
         public Difficulty Difficulty { get; set; } // EASY, NORMAL, HARD
         public DateTime DatePerformed { get; set; } // MM/DD/YYYY
-        public Duration Duration { get; set; } // do I want to return DateTime or String?
+
+    }
+
+    class Weights : Exercise
+    {
+
+        public int Reps { get; set; }
+        public int Sets { get; set; }
+        public double Pounds { get; set; }
+
+
+
+        public Weights(string name, string startTime, string endTime, int reps, int sets, double pounds, DateTime datePerformed, Difficulty difficulty, Duration duration)
+        {
+            this.Name = name;
+            this.Type = "Weights";
+            this.Difficulty = difficulty;
+            this.DatePerformed = datePerformed;
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+            this.Reps = reps;
+            this.Sets = sets;
+            this.Pounds = pounds;
+        }
+
+    }
+
+    class Cardio : Exercise
+    {
+
+        public double Miles { get; set; }
+        public int Steps { get; set; }
+        public int CaloriesBurned { get; set; }
+
+
+        public Cardio(string name, DateTime datePerformed, Difficulty difficulty, string startTime, string endTime, int caloriesBurned, double Miles = 12.23, int Steps = 0)
+        {
+            this.Name = name;
+            this.Type = "Cardio";
+            this.Difficulty = difficulty;
+            this.DatePerformed = datePerformed;
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+            this.CaloriesBurned = caloriesBurned;
+        }
 
 
 
     }
+
+
 
 
 
--- P0_WorkoutLog/WorkoutLog.cs
+++ P0_WorkoutLog/WorkoutLog.cs
@@ -1,4 +1,21 @@
-﻿
+﻿using System;
+using Exercises;
 
 // How to structure a workout?
 // Day, Week, Month, etc..?
+
+
+
+namespace Test
+{
+
+
+    class Testing
+    {
+
+        public static void Main(string[] args)
+        {
+            Exercise e = new Cardio();
+        }
+    }
+}
