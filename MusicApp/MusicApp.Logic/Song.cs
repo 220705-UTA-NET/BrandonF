b733b3ec2783f739e30b8a4939d65b32acbf0f8b
Brandon <figueredo407@gmail.com>
2022-07-25 16:41:47 -0400

    got work done

---
+++ MusicApp/MusicApp.Logic/Song.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicApp.Logic
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Artist { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ MusicApp/MusicApp/Controllers/SongController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace MusicApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongController : ControllerBase
+    {
+        private readonly IRepository repo;
+        private readonly ILogger<SongController> logger;
+
+        public SongController(IRepository repo, ILogger<SongController> logger)
+        {
+            this.repo = repo;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SongController>>> GetAllSongs()
+        {
+            IEnumerable<SongController> songs;
+            try
+            {
+                await repo.GetAll();
+            }
+            catch (Exception)
+            {
+                this.logger.LogError(e, e.message);
+                this.logger.LogInformation("Song Controller couldn't run GetAll method");
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("")]
+        public ContentResult Testing()
+        {
+            return Content("Message received. Sending a response back");
+        }
+
+        [HttpGet("{title}")]
+        public ActionResult GetSong(string title)
+        {
+            return View("/html/test.html");
+        }
+    }
+}
