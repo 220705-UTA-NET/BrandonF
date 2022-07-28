4220a14756aaf991bc05e71dbf26d49846fa9fc9
Brandon <figueredo407@gmail.com>
2022-07-28 19:16:56 -0400

    push

--- MusicAppConsole/Listener.cs
+++ MusicAppConsole/Listener.cs
@@ -16,34 +16,23 @@ namespace MusicApclep.App
         public HttpClient _httpClient;
         public readonly string serverURL = "https://localhost:7257/api/";
 
-
-
         public Listener()
         {
             _httpClient = new HttpClient();
 
         }
 
-        // public void AddSong(SongDTO song)
-        // {
-
-        // }
-
-        // public List<SongDTO> GetFavorites()
-        // {
-
-        // }
-
-
         static async Task Main(string[] args)
         {
-
 
             string dbURL = File.ReadAllText("C:/Users/brand/Revature/connection.txt");
 
             Listener user = new Listener();
             user.StartServer(user.serverURL);
             string? choice = "";
+
+
+
 
             // loop that lets the user continuously make API calls
             while (choice != "-1")
@@ -63,6 +52,9 @@ namespace MusicApclep.App
                         var song = await user.GetSongRequest(title, artist);
                         Console.WriteLine(song.ToString());
                         break;
+                    case "2":
+                        await user.InsertSongRequest();
+                        break;
                     case "-1":
                         Console.WriteLine("Exiting...");
                         break;
@@ -71,10 +63,6 @@ namespace MusicApclep.App
                 }
             }
         }
-
-        // public
-
-
         // method starts the server
         public void StartServer(string serverURL)
         {
@@ -85,10 +73,41 @@ namespace MusicApclep.App
             ); // set the request header content type to Json
         }
 
-        public async Task<string> GetSongRequest(string name, string artist)
+        public async Task InsertSongRequest()
         {
 
-            HttpResponseMessage response = await _httpClient.GetAsync($"song/{name}/{artist}");
+            SongDTO song = new SongDTO("FromConsole", "App", "EmptyAlbum");
+            string toInsert = Newtonsoft.Json.JsonConvert.SerializeObject(song);
+
+            StringContent toSend = new StringContent(toInsert, System.Text.Encoding.UTF8, "application/json");
+
+            try
+            {
+                // send a post request with the serialized song object
+                HttpResponseMessage response = await _httpClient.PostAsync("Song/addsong", toSend);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("status code is find");
+                }
+                else
+                {
+                    Console.WriteLine("error status code");
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                // todo
+                Console.WriteLine("Couldn't insert");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        public async Task<string> GetSongRequest(string name, string artist)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"Song/{name}/{artist}");
 
             if (response.IsSuccessStatusCode)
             {
@@ -96,7 +115,7 @@ namespace MusicApclep.App
                 {
                     string? obj = await response.Content.ReadAsStringAsync();
                     SongDTO? song = JsonConvert.DeserializeObject<SongDTO>(obj);
-                    return FormatSong(song);
+                    // return FormatSong(song);
                 }
                 catch (Exception ex)
                 {
@@ -112,13 +131,13 @@ namespace MusicApclep.App
         }
 
         // method will format a song
-        public string FormatSong(SongDTO song)
-        {
-            Console.WriteLine(song.Artist);
-            StringBuilder s = new StringBuilder();
-            s.Append($"ID: {song.Id}\nTitle: {song.Name}\nArtist: {song.Artist}\nAlbum: {song.Album}\n");
+        // public string FormatSong(SongDTO song)
+        // {
+        //     Console.WriteLine(song.Artist);
+        //     StringBuilder s = new StringBuilder();
+        //     s.Append($"ID: {song.Id}\nTitle: {song.Name}\nArtist: {song.Artist}\nAlbum: {song.Album}\n");
 
-            return s.ToString();
-        }
+        //     return s.ToString();
+        // }
     }
 }
--- MusicAppConsole/SongDTO.cs
+++ MusicAppConsole/SongDTO.cs
@@ -4,16 +4,15 @@ namespace MusicClasses
 {
     public class SongDTO
     {
-        public int Id { get; set; }
+        // public int Id { get; set; }
         public string Name { get; set; }
         public string Artist { get; set; }
         public string? Album { get; set; }
 
         public SongDTO() { }
 
-        public SongDTO(int id, string name, string artist, string? album)
+        public SongDTO(string name, string artist, string? album)
         {
-            Id = id;
             Name = name;
             Artist = artist;
             Album = album;
